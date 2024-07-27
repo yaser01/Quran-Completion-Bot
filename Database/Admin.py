c319c2b5bbec3709c646073b82b28515c90be4c1
Yasser Milato <yasser.milato@gmail.com>
2024-07-27 18:15:12 +0300

    tiny change

--- Database/Admin.py
+++ Database/Admin.py
@@ -69,7 +69,7 @@ class KhatmaAdmin(ModelView, model=Khatma):
     name_plural = "Khatmas"
     column_list = [Khatma.id, Khatma.User,Khatma.user_id, Khatma.name_of_opener, Khatma.time, Khatma.description,
                    Khatma.number_of_days_to_finish_a_part]
-    column_details_list = [Khatma.id, Khatma.User, Khatma.name_of_opener, Khatma.time, Khatma.description,
+    column_details_list = [Khatma.id, Khatma.User,Khatma.user_id, Khatma.name_of_opener, Khatma.time, Khatma.description,
                            Khatma.number_of_days_to_finish_a_part]
     column_searchable_list = [Khatma.id, Khatma.user_id]
     column_export_list = [Khatma.id, Khatma.User, Khatma.name_of_opener, Khatma.time, Khatma.description,
